151db584cf3c51982337fae322f3e7df98044a0c
adesina o. marvelous <adesinaolaoluwamarvelous@gmail.com>
2024-01-20 14:56:13 -0800

    first commit

---
+++ portfolio/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.forms import ModelForm
+from .models import ContactModel
+
+
+class CONTACTFORM(forms.ModelForm):
+    class Meta:
+        model = ContactModel
+        fields = ('name', 'subject','email', 'messagess')
+        exclude = ['created']
+        widgets={
+            'name': forms.TextInput(attrs={'class':"form-control"}),
+            'email': forms.TextInput(attrs={'class':"form-control"}),
+            'messagess': forms.TextInput(attrs={'class':"form-control"}),
+            
+        }
+
